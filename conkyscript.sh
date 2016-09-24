5d8e31bf118be684b506e09b01e776ee39c1a41f
Sudarshan-ArchLinux <sudar.theone@gmail.com>
2016-09-24 19:42:47 +0530

    first commit

---
+++ conkyscript.sh
@@ -0,0 +1,13 @@
+conky -c ~/.conky/conky_cal &
+conky -c ~/.conky/conky_asset &
+conky -c ~/.conky/conky.conf &
+conky -c ~/.conky/conky_cpus &
+conky -c ~/.conky/conky_hdd &
+conky -c ~/.conky/conky_weather &
+conky -c ~/.conky/conky_loggs &
+conky -c ~/.conky/conky_processes &
+conky -c ~/.conky/conky_sensors &
+conky -c ~/.conky/conky_connections &
+conky -c ~/.conky/conky_clock &
+conky -c ~/.conky/conky_hackerNews &
+~/.conky/.gifbg ~/.conky/quark.gif &
